0e998ba748ddc2d4edae161335a67485834b1051
Peter Pi <peter@Peters-MacBook-Pro.local>
2022-01-05 23:08:23 -0800

    Optimize and fix translation bubble not showing up

--- background.js
+++ background.js
@@ -1,16 +1,26 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.contentText == "text") {
+    if (request.contentText == "englishText") {
             var url = "https://dictionary.cambridge.org/us/dictionary/english-chinese-traditional/" + encodeURIComponent(request.txt);
-            var url2 = "https://googledictionaryapi.eu-gb.mybluemix.net/?define=" + encodeURIComponent(request.txt);
-            var text = '';
+            var url2 = "https://api.dictionaryapi.dev/api/v2/entries/en/" + encodeURIComponent(request.txt);
+            var obj = {};
+            fetch(url2)
+                .then(response => response.json())
+                .then(result => {
+                    obj['english'] = result[0] && result[0]['meanings'] && result[0]['meanings'][0] && result[0]['meanings'][0]['definitions'] && 
+                                     result[0]['meanings'][0]['definitions'][0] && result[0]['meanings'][0]['definitions'][0]['definition'] || '';
+                })
+                .catch(error => console.log(error))
             fetch(url)
                 .then(response => response.text())
-                .then(result => text += result + "divider__")
+                .then(result => {
+                    const doc = document.createElement("div");
+                    doc.innerHTML = result;
+                    let res = doc.querySelectorAll('.def-body')[0] && doc.querySelectorAll('.def-body')[0].firstElementChild && doc.querySelectorAll('.def-body')[0].firstElementChild.innerText || '';
+                    obj['chinese'] = res
+                    sendResponse(obj)
+                })
                 .catch(error => console.log(error))
-            fetch(url2)
-                .then(response => response.text())
-                .then(result => sendResponse(text + result))
-                .catch(error => console.log(error))
+            
             return true;
     }
 });
--- content.js
+++ content.js
@@ -41,12 +41,12 @@ window.onload = function () {
                 return false;
             }
         }
-        
-        // Helper function to get the English definition 
-        function getSecondPart(str) {
-            return str.split('divider__')[1];
-        }
 
+        // Helper function to get the English definition 
+        // function getSecondPart(str) {
+        //     return str.split('divider__')[1];
+        // }
+        
         // Text selected, triggers the search
         $(document).on('mouseup', 'div, span, p', function(event) {
             selectedText = getSelectedText();
@@ -77,35 +77,17 @@ window.onload = function () {
                         // New word to search for
                         else {
                             chrome.runtime.sendMessage({
-                                contentText: "text", txt: selectedText.toString().toLowerCase().trim()
+                                contentText: "englishText", txt: selectedText.toString().toLowerCase().trim()
                             }, function (result) {
                                 // English
-                                let def = '';
-                                let eng_text = getSecondPart(result);
-                                if (eng_text) {
-                                    eng_match = /OOPS/g.exec(eng_text);
-                                }
+                                let english = result['english'] || 'Could not translate the selected text';
+                                let chinese = result['chinese'] || '很抱歉，字典搜尋不到您選取的字';
+                                console.log(english)
+                                console.log(chinese)
         
-                                // English definition
-                                if (eng_match == null || eng_match[0] != "OOPS") {
-                                    def = eng_text.match(new RegExp("\"definition\": \"" + "(.*)" + ".\""))[1];
-                                }
-                                else {
-                                    def = 'Could not translate the selected text';
-                                }
-        
-                                // Traditional Chinese
-                                var res = /<span class="trans dtrans dtrans-se.*?>(.*?)<\/span>/g.exec(result);
-                                if(res) {
-                                    res = res[1];
-                                }
-                                else {
-                                    res = '很抱歉，字典搜尋不到您選取的字';
-                                }
-                                
                                 // Traditional Chinese & English
-                                engDiv.text(def);
-                                cnDiv.text(res);
+                                engDiv.text(english);
+                                cnDiv.text(chinese);
                                 $speechBubble.css({top: event.pageY - ($speechBubble.height()/2), left: event.pageX});
                                 $speechBubble.show();
 
@@ -118,15 +100,15 @@ window.onload = function () {
                                     let obj_eng = userKeyIds.find(o => o[eng]);
                                     let obj_ch = userKeyIds.find(o => o[ch]);
                                     if(obj_eng === undefined) {
-                                        userKeyIds.push({[eng]: def});
-                                        userKeyIds.push({[ch]: res});
+                                        userKeyIds.push({[eng]: english});
+                                        userKeyIds.push({[ch]: chinese});
                                         // set the new array value to the same key
                                         chrome.storage.local.set({userKeyIds: userKeyIds}, function () {});
                                     }
-                                    else if(obj_eng == 'Could not Translate the selected text' && def != 'Could not Translate the selected text' ||
-                                            obj_ch == '很抱歉，字典搜尋不到您選取的字' && res != '很抱歉，字典搜尋不到您選取的字') {
-                                        userKeyIds.push({[eng]: def});
-                                        userKeyIds.push({[ch]: res});
+                                    else if(obj_eng == 'Could not Translate the selected text' && english != 'Could not Translate the selected text' ||
+                                            obj_ch == '很抱歉，字典搜尋不到您選取的字' && chinese != '很抱歉，字典搜尋不到您選取的字') {
+                                        userKeyIds.push({[eng]: english});
+                                        userKeyIds.push({[ch]: chinese});
                                         // set the new array value to the same key
                                         chrome.storage.local.set({userKeyIds: userKeyIds}, function () {});
                                     }
@@ -151,78 +133,3 @@ window.onload = function () {
 
     });
 }
-
-// Optional: with parts of speech (noun, verb, adjective, etc.)
-// var htmlContent = '';
-// var englishContent = '';
-// var eng_dict = {};
-// if (eng_match == null || eng_match[0] != "OOPS") {
-//     //English translation
-//     let english_text = JSON.parse(eng_text);
-//     var eng_title = Object.keys(english_text[0].meaning)[0];
-//     let eng_val = "";
-//     if (english_text[0].meaning) {
-//         if (english_text[0].meaning.noun) {
-//             eng_val = english_text[0].meaning.noun[0].definition;
-//         }
-//         else if (english_text[0].meaning.verb) {
-//             eng_val = english_text[0].meaning.verb[0].definition;
-//         }
-//         else if (english_text[0].meaning.adjective) {
-//             eng_val = english_text[0].meaning.adjective[0].definition;
-//         }
-//         else if (english_text[0].meaning.pronoun) {
-//             eng_val = english_text[0].meaning.pronoun[0].definition;
-//         }
-//         else if (english_text[0].meaning.adverb) {
-//             eng_val = english_text[0].meaning.adverb[0].definition;
-//         }
-//         else if (english_text[0].meaning.preposition) {
-//             eng_val = english_text[0].meaning.preposition[0].definition;
-//         }
-//         else if (english_text[0].meaning.abbreviation) {
-//             eng_val = english_text[0].meaning.abbreviation[0].definition;
-//         }
-//         else if (english_text[0].meaning.conjunction) {
-//             eng_val = english_text[0].meaning.conjunction[0].definition;
-//         }
-//         else if (english_text[0].meaning.exclamation) {
-//             eng_val = english_text[0].meaning.exclamation[0].definition;
-//         }
-//         eng_dict[eng_title] = eng_val;
-//     }
-// }
-// else {
-//     englishContent = 'Could not translate the selected text';
-// }
-
-// Traditional Chinese
-// cn_title_regex = /<div class=" pos_button fz-14 fl-l mr-12">(.*?)<\/div>/g;
-// cn_val_regex = /<div class=" fz-16 fl-l dictionaryExplanation">(.*?)<\/div>/g;
-// var cn_matches = {};
-// var cn_title_match = cn_title_regex.exec(result);
-// var cn_val_match = cn_val_regex.exec(result);
-
-// while (cn_val_match != undefined) {
-
-//     // Chinese translation
-//     if (cn_title_match == undefined) {
-//         cn_matches[""] = cn_val_match[1];
-//         cn_title_match = cn_title_regex.exec(result);
-//         cn_val_match = cn_val_regex.exec(result);
-//     }
-//     else {
-//         cn_matches[cn_title_match[1]] = cn_val_match[1];
-//         cn_title_match = cn_title_regex.exec(result);
-//         cn_val_match = cn_val_regex.exec(result);
-//     }
-// }
-
-// for (var key in cn_matches) {
-//     // console.log(key, cn_matches[key]);
-//     htmlContent += key + " " + cn_matches[key];
-// }
-
-// for (var key in eng_dict) {
-//     englishContent += key + ". " + eng_dict[key];
-// }
